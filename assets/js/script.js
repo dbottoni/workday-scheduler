95fbc5668b5fce1554b09f026c7dd336d90fce46
Dana Bottoni <danabottoni@gmail.com>
2021-06-19 17:47:41 -0400

    append date to header

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function insertDate(date) {  
+  let h3= document.createElement('h3');
+  h3.textContent = date;
+  return h3;
+}
+
+const currentDay = document.querySelector('#currentDay');
+currentDay.appendChild(insertDate(new Date()));
+
